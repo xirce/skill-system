38dccdad4e4727eb3d17544aa50be1dddb7338b0
xirce <dgorohov78@gmail.com>
2022-11-13 20:41:13 +0500

    Добавлен пустой IdentityServer4 шаблон

---
+++ SkillSystem.IdentityServer4/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace SkillSystem.IdentityServer4;
+
+public class Startup
+{
+    private IWebHostEnvironment Environment { get; }
+
+    public Startup(IWebHostEnvironment environment)
+    {
+        Environment = environment;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // uncomment, if you want to add an MVC-based UI
+        // services.AddControllersWithViews();
+
+        services.AddIdentityServer()
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryApiResources(Config.ApiResources)
+            .AddInMemoryClients(Config.Clients)
+            .AddDeveloperSigningCredential();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // uncomment if you want to add MVC
+        // app.UseStaticFiles();
+        // app.UseRouting();
+
+        app.UseIdentityServer();
+
+        // uncomment, if you want to add MVC
+        // app.UseAuthorization();
+        // app.UseEndpoints(endpoints =>
+        // {
+        //     endpoints.MapDefaultControllerRoute();
+        // });
+    }
+}
