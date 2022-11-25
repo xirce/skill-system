842f2fb1c562bab0e539e135cf94d7fb9903b707
xirce <dgorohov78@gmail.com>
2022-11-25 15:40:47 +0500

    Добавлена сущность Skill и конфигурация для неё

---
+++ SkillSystem.Core/Entities/Skill.cs
@@ -0,0 +1,31 @@
+﻿using SkillSystem.Core.Enums;
+
+namespace SkillSystem.Core.Entities;
+
+/// <summary>
+/// Знание/понимание чего-либо.
+/// </summary>
+public class Skill : BaseEntity
+{
+    /// <summary>
+    /// Название знания.
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Тип знания.
+    /// </summary>
+    public SkillType Type { get; set; }
+
+    /// <summary>
+    /// Группа знаний (тема), которой принадлежит это знание.
+    /// </summary>
+    public Skill? Group { get; set; }
+
+    public int? GroupId { get; set; }
+
+    /// <summary>
+    /// Набор знаний, объединенных темой этого знания.
+    /// </summary>
+    public ICollection<Skill> SubSkills { get; set; }
+}
---
+++ SkillSystem.Infrastructure/Persistence/SkillSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillSystem.Core.Entities;
+
+namespace SkillSystem.Infrastructure.Persistence;
+
+public class SkillSystemDbContext : DbContext
+{
+    public DbSet<Skill> Skills { get; set; }
+
+    public SkillSystemDbContext(DbContextOptions<SkillSystemDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Skill>()
+            .HasOne(skill => skill.Group)
+            .WithMany(group => group.SubSkills)
+            .HasForeignKey(skill => skill.GroupId);
+    }
+}
