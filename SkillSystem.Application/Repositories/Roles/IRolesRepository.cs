72772d3d3ed0573fd6ff64182d1ec38ec47f14a1
xirce <dgorohov78@gmail.com>
2022-11-28 18:00:03 +0500

    Реализован репозиторий для сущности Role

---
+++ SkillSystem.Application/Repositories/Roles/IRolesRepository.cs
@@ -0,0 +1,12 @@
+﻿using SkillSystem.Core.Entities;
+
+namespace SkillSystem.Application.Repositories.Roles;
+
+public interface IRolesRepository
+{
+    Task<int> CreateRoleAsync(Role role);
+    Task<Role?> FindRoleByIdAsync(int roleId);
+    IQueryable<Role> FindRoles(string? title = default);
+    Task UpdateRoleAsync(Role role);
+    Task DeleteRoleAsync(int roleId);
+}
