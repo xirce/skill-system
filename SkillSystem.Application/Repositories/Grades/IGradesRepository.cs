0d4d9a96efd4964495750191315b0dcd18f208db
xirce <dgorohov78@gmail.com>
2022-12-05 18:17:23 +0500

    Добавлен репозиторий для сущности Grade

---
+++ SkillSystem.Application/Repositories/Grades/IGradesRepository.cs
@@ -0,0 +1,14 @@
+﻿using SkillSystem.Core.Entities;
+
+namespace SkillSystem.Application.Repositories.Grades;
+
+public interface IGradesRepository
+{
+    Task<Grade?> FindGradeByIdAsync(int gradeId);
+    Task<Grade> GetGradeByIdAsync(int gradeId);
+    IQueryable<Grade> FindGrades(string? title = default);
+    Task<IEnumerable<Skill>> GetGradeSkillsAsync(int gradeId);
+    Task UpdateGradeAsync(Grade grade);
+    Task AddGradeSkillAsync(int gradeId, Skill skill);
+    Task DeleteGradeSkillAsync(int gradeId, int skillId);
+}
