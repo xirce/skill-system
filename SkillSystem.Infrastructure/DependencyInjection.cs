a30f575d7cecb11c77a3a2f59b84d88eca520efa
xirce <dgorohov78@gmail.com>
2022-12-10 21:02:32 +0500

    К WebApi подключён Postgres, добавлена инициализация базы на старте приложения

--- SkillSystem.Infrastructure/DependencyInjection.cs
+++ SkillSystem.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using SkillSystem.Application.Repositories.Grades;
 using SkillSystem.Application.Repositories.Roles;
 using SkillSystem.Application.Repositories.Skills;
@@ -10,9 +12,12 @@ namespace SkillSystem.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<SkillSystemDbContext>(options => options.UseInMemoryDatabase("SkillSystem"));
+        services.AddDbContext<SkillSystemDbContext>(
+            options => options.UseNpgsql(configuration.GetConnectionString(nameof(SkillSystem)))
+        );
+        services.AddScoped<SkillSystemDbInitializer>();
 
         services.AddScoped<ISkillsRepository, SkillsRepository>();
         services.AddScoped<IRolesRepository, RolesRepository>();
---
+++ SkillSystem.Infrastructure/Persistence/SkillSystemDbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace SkillSystem.Infrastructure.Persistence;
+
+public class SkillSystemDbInitializer
+{
+    private readonly SkillSystemDbContext dbContext;
+    private readonly ILogger<SkillSystemDbInitializer> logger;
+
+    public SkillSystemDbInitializer(SkillSystemDbContext dbContext, ILogger<SkillSystemDbInitializer> logger)
+    {
+        this.dbContext = dbContext;
+        this.logger = logger;
+    }
+
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            await dbContext.Database.MigrateAsync();
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, "An error occurred during database initialization");
+            throw;
+        }
+    }
+}
