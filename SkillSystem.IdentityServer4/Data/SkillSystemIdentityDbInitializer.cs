ff3741715788d9058748cb70670161560e572f46
xirce <dgorohov78@gmail.com>
2022-12-12 22:13:52 +0500

    IdentityServer переведён с InMemory бд на PostgreSQL, добавлена инициализация бд

---
+++ SkillSystem.IdentityServer4/Data/SkillSystemIdentityDbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace SkillSystem.IdentityServer4.Data;
+
+public class SkillSystemIdentityDbInitializer
+{
+    private readonly SkillSystemIdentityDbContext dbContext;
+
+    public SkillSystemIdentityDbInitializer(SkillSystemIdentityDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            await dbContext.Database.MigrateAsync();
+        }
+        catch (Exception exception)
+        {
+            Log.Error(exception, "An error occurred during database initialization");
+            throw;
+        }
+    }
+}
