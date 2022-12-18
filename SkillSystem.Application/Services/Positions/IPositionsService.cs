b9185d677905e5058b0720679ff72c81c9784a6e
xirce <dgorohov78@gmail.com>
2022-12-18 17:32:10 +0500

    Реализован сервис для должностей

---
+++ SkillSystem.Application/Services/Positions/IPositionsService.cs
@@ -0,0 +1,15 @@
+﻿using SkillSystem.Application.Common.Models.Requests;
+using SkillSystem.Application.Common.Models.Responses;
+using SkillSystem.Application.Repositories.Positions.Filters;
+using SkillSystem.Application.Services.Positions.Models;
+
+namespace SkillSystem.Application.Services.Positions;
+
+public interface IPositionsService
+{
+    Task<int> CreatePositionAsync(PositionRequest request);
+    Task<PositionResponse> GetPositionByIdAsync(int positionId);
+    Task<PaginatedResponse<PositionResponse>> FindPositionsAsync(PaginationQuery<PositionFilter> query);
+    Task UpdatePositionAsync(int positionId, PositionRequest request);
+    Task DeletePositionAsync(int positionId);
+}
---
+++ SkillSystem.Application/Services/Positions/Models/PositionRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillSystem.Application.Services.Positions.Models;
+
+public record PositionRequest
+{
+    [Required]
+    [MaxLength(30)]
+    public string Title { get; init; }
+}
