8f150d4cb9f3833884f9f96d7dd136d4860e7c96
xirce <dgorohov78@gmail.com>
2022-12-18 20:09:19 +0500

    GradeRequest-у добавлена валидация

--- SkillSystem.Application/Services/Grades/Models/GradeRequest.cs
+++ SkillSystem.Application/Services/Grades/Models/GradeRequest.cs
@@ -1,6 +1,10 @@
-﻿namespace SkillSystem.Application.Services.Grades.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillSystem.Application.Services.Grades.Models;
 
 public record GradeRequest
 {
+    [Required]
+    [MaxLength(30)]
     public string Title { get; init; }
 }
