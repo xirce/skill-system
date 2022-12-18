049ad1adf9f11bf52c205d51516504cc89d7139a
xirce <dgorohov78@gmail.com>
2022-12-18 14:11:13 +0500

    Реализован репозиторий для обязанностей

---
+++ SkillSystem.Application/Repositories/Duties/IDutiesRepository.cs
@@ -0,0 +1,14 @@
+﻿using SkillSystem.Application.Repositories.Duties.Filters;
+using SkillSystem.Core.Entities;
+
+namespace SkillSystem.Application.Repositories.Duties;
+
+public interface IDutiesRepository
+{
+    Task<int> CreateDutyAsync(Duty duty);
+    Task<Duty?> FindDutyByIdAsync(int dutyId);
+    Task<Duty> GetDutyByIdAsync(int dutyId);
+    IQueryable<Duty> FindDuties(DutyFilter? filter = default);
+    Task UpdateDutyAsync(Duty duty);
+    Task DeleteDutyAsync(int dutyId);
+}
