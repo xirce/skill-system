9f03ffec767ba95987f8a9d6404a97d7373d137a
xirce <dgorohov78@gmail.com>
2022-12-18 18:10:46 +0500

    Добавлена миграция с должностью

--- SkillSystem.Infrastructure/Migrations/20221218125847_AddPositionMigration.Designer.cs
+++ SkillSystem.Infrastructure/Migrations/20221218125847_AddPositionMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SkillSystem.Infrastructure.Persistence;
@@ -11,9 +12,10 @@ using SkillSystem.Infrastructure.Persistence;
 namespace SkillSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(SkillSystemDbContext))]
-    partial class SkillSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221218125847_AddPositionMigration")]
+    partial class AddPositionMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SkillSystem.Infrastructure/Migrations/20221218125847_AddPositionMigration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SkillSystem.Infrastructure.Migrations
+{
+    public partial class AddPositionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PositionGrades",
+                columns: table => new
+                {
+                    PositionId = table.Column<int>(type: "integer", nullable: false),
+                    GradeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionGrades", x => new { x.PositionId, x.GradeId });
+                    table.ForeignKey(
+                        name: "FK_PositionGrades_Grades_GradeId",
+                        column: x => x.GradeId,
+                        principalTable: "Grades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PositionGrades_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionGrades_GradeId",
+                table: "PositionGrades",
+                column: "GradeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PositionGrades");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+        }
+    }
+}
