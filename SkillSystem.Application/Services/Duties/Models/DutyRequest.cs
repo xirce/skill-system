556cc78c5bd4c66a82441277ea87e7d3274f6959
xirce <dgorohov78@gmail.com>
2022-12-25 19:16:37 +0500

    Реализован сервис обязанностей

---
+++ SkillSystem.Application/Services/Duties/Models/DutyRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillSystem.Application.Services.Duties.Models;
+
+public record DutyRequest
+{
+    [Required]
+    [MaxLength(30)]
+    public string Title { get; init; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; init; }
+}
