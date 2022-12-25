59cb527858796c357c5c97b17584707dc4ab0dbb
xirce <dgorohov78@gmail.com>
2022-12-25 20:04:13 +0500

    Добавлена миграция с обязанностью

--- SkillSystem.Infrastructure/Migrations/20221225150237_AddDutyMigration.Designer.cs
+++ SkillSystem.Infrastructure/Migrations/20221225150237_AddDutyMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SkillSystem.Infrastructure.Persistence;
@@ -11,9 +12,10 @@ using SkillSystem.Infrastructure.Persistence;
 namespace SkillSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(SkillSystemDbContext))]
-    partial class SkillSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221225150237_AddDutyMigration")]
+    partial class AddDutyMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
