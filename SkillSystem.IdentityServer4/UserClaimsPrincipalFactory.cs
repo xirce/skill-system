4cb8b2c347b6d308788e4751daf921b2e3d64e13
xirce <dgorohov78@gmail.com>
2022-12-26 03:25:11 +0500

    Добавлена кастомная фабрика клэймов с фио
    Значение клэйма name заменено с Email на имя + фамилию

---
+++ SkillSystem.IdentityServer4/UserClaimsPrincipalFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SkillSystem.IdentityServer4.Models;
+
+namespace SkillSystem.IdentityServer4;
+
+public class UserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+{
+    public UserClaimsPrincipalFactory(
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IOptions<IdentityOptions> options
+    ) : base(userManager, roleManager, options)
+    {
+    }
+
+    protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+    {
+        var identity = await base.GenerateClaimsAsync(user);
+
+        identity.AddClaim(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+        identity.AddClaim(new Claim(JwtClaimTypes.FamilyName, user.LastName));
+        identity.AddClaim(new Claim(JwtClaimTypes.MiddleName, user.Patronymic));
+
+        var nameClaim = identity.FindFirst(claim => claim.Type == JwtClaimTypes.Name);
+        identity.RemoveClaim(nameClaim);
+        identity.AddClaim(new Claim(JwtClaimTypes.Name, $"{user.FirstName} {user.LastName}"));
+
+        return identity;
+    }
+}
