503b12bcf66cf37c03ad02bf6254e52ceadb9e4f
xirce <dgorohov78@gmail.com>
2022-12-26 19:05:33 +0500

    Добавлена миграция со скиллами сотрудника

---
+++ SkillSystem.Infrastructure/Migrations/20221226140249_AddEmployeeSkillMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillSystem.Infrastructure.Migrations
+{
+    public partial class AddEmployeeSkillMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeSkills",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<string>(type: "text", nullable: false),
+                    SkillId = table.Column<int>(type: "integer", nullable: false),
+                    IsApproved = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeSkills", x => new { x.EmployeeId, x.SkillId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeSkills_Skills_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeSkills_SkillId",
+                table: "EmployeeSkills",
+                column: "SkillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeSkills");
+        }
+    }
+}
