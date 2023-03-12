8ba9ff2f77e5d52c2ea2fe226d9ae13c2d6e863e
xirce <dgorohov78@gmail.com>
2023-03-12 18:31:45 +0500

    Добавлена связка сотрудник-руководитель и конфигурация для неё

---
+++ SkillSystem.Core/Entities/ManagerSubordinate.cs
@@ -0,0 +1,7 @@
+﻿namespace SkillSystem.Core.Entities;
+
+public class ManagerSubordinate
+{
+    public Guid ManagerId { get; set; }
+    public Guid SubordinateId { get; set; }
+}
--- SkillSystem.Infrastructure/Persistence/SkillSystemDbContext.cs
+++ SkillSystem.Infrastructure/Persistence/SkillSystemDbContext.cs
@@ -14,6 +14,7 @@ public class SkillSystemDbContext : DbContext
     public DbSet<PositionGrade> PositionGrades { get; set; }
     public DbSet<PositionDuty> PositionDuties { get; set; }
     public DbSet<EmployeeSkill> EmployeeSkills { get; set; }
+    public DbSet<ManagerSubordinate> ManagersSubordinates { get; set; }
 
     public SkillSystemDbContext()
     {
@@ -117,5 +118,8 @@ public class SkillSystemDbContext : DbContext
             .HasOne(skill => skill.Skill)
             .WithMany()
             .HasForeignKey(skill => skill.SkillId);
+
+        modelBuilder.Entity<ManagerSubordinate>()
+            .HasKey(managerSubordinate => new { managerSubordinate.ManagerId, managerSubordinate.SubordinateId });
     }
 }
