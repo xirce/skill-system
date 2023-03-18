c8b0b3fb410175a300b4f987de5b6ebc009f72d0
xirce <dgorohov78@gmail.com>
2023-03-19 01:14:15 +0500

    Добавлена миграция с руководителем

---
+++ SkillSystem.Infrastructure/Migrations/20230318195444_AddManagerMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillSystem.Infrastructure.Migrations
+{
+    public partial class AddManagerMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ManagersSubordinates",
+                columns: table => new
+                {
+                    ManagerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubordinateId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManagersSubordinates", x => new { x.ManagerId, x.SubordinateId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ManagersSubordinates");
+        }
+    }
+}
