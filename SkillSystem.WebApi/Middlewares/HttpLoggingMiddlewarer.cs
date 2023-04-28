4621d6f7d4b6491d2dfd1e2a758ebaf586264e2a
xirce <dgorohov78@gmail.com>
2023-04-29 04:35:53 +0500

    Добавлено логирование HTTP запросов, подключен Serilog

---
+++ SkillSystem.WebApi/Middlewares/HttpLoggingMiddlewarer.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Serilog;
+
+namespace SkillSystem.WebApi.Middlewares;
+
+public class HttpLoggingMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public HttpLoggingMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        LogRequest(context.Request);
+
+        await next(context);
+
+        LogResponse(context.Response, stopwatch.Elapsed);
+    }
+
+    private void LogRequest(HttpRequest request)
+    {
+        Log.Information("Received request {Method} {Path}{Query}", request.Method, request.Path, request.QueryString);
+    }
+
+    private void LogResponse(HttpResponse response, TimeSpan elapsed)
+    {
+        Log.Information("Response code = {ResponseCode}. Elapsed time = {ElapsedTime}.", response.StatusCode, elapsed);
+    }
+}
