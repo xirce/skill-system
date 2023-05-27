8909357040573b994904313efe9a273114f883a9
xirce <dgorohov78@gmail.com>
2023-05-28 02:14:24 +0500

    Добавлена миграция с грейдами сотрудников

---
+++ SkillSystem.Infrastructure/Migrations/20230527163256_AddEmployeeGradeMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillSystem.Infrastructure.Migrations
+{
+    public partial class AddEmployeeGradeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeGrades",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    GradeId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeGrades", x => new { x.EmployeeId, x.GradeId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeGrades_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeGrades_Grades_GradeId",
+                        column: x => x.GradeId,
+                        principalTable: "Grades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeGrades_GradeId",
+                table: "EmployeeGrades",
+                column: "GradeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeGrades");
+        }
+    }
+}
